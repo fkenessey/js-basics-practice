1c7f72c765bb27d358b835aad4758dde5916b607
Szász Tibor <tszasz@gmail.com>
2018-02-15 12:09:09 +0100

    ajax++

--- hello.js
+++ hello.js
@@ -2,6 +2,10 @@ const os = require('os');
 let express = require('express');
 let app = express();
 
+app.get('/', function( request, response ){
+    response.sendFile( __dirname + '/ajax.html' );
+});
+
 app.get('/greenfox', function( request, response ){
     let colors = ['red','green','blue'];
     response.json(colors);
