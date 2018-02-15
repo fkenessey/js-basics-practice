aea2b872592defae8c888c21bf03f761e7e63122
Szász Tibor <tszasz@gmail.com>
2018-02-15 11:51:16 +0100

    node express

--- hello.js
+++ hello.js
@@ -1,3 +1,14 @@
 const os = require('os');
+let express = require('express');
+let app = express();
 
-console.log('hello from node', os.platform() );
+app.get('/greenfox', function( request, response ){
+    let stuff = {
+        'location': 'greenfox'
+    }
+    response.json(stuff).status(201);
+});
+
+app.listen( 1234, function(){
+    console.log('hello from node on port 1234'); 
+});
